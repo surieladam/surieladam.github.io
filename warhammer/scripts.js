c0706fd248bcecb58f2300497a236a8ba6926454
ADAM Suriel <surieladam@gmail.com>
2025-12-14 19:52:19 +0000

    fix hover effects

--- warhammer/scripts.js
+++ warhammer/scripts.js
@@ -1,10 +1,6 @@
 // Képre kattintva nagyítás
 document.querySelectorAll('img').forEach(img => {
     img.addEventListener('click', function() {
-        if(this.style.width === '300px') {
-            this.style.width = '100px';
-        } else {
-            this.style.width = '300px';
-        }
+        this.classList.toggle('enlarged');
     });
 });
